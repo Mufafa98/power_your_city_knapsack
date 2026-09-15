39b03da1c56b867dfc7a07f24f9b028c74a87071
Mufafa98 <marian.parlea2@gmail.com>
2026-09-15 11:23:35 +0300

    Add plot, mutation operator and multi threded gui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use ::rand::{RngExt, SeedableRng, rngs::StdRng};
 use macroquad::prelude::*;
-use std::{collections::HashMap, iter};
+use std::{
+    collections::HashMap,
+    iter,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 #[derive(Clone, Copy)]
 struct GeneratorData {
@@ -45,6 +51,7 @@ impl From<GeneratorData> for Gene {
 #[derive(Clone)]
 struct Chromosome {
     genes: Vec<Gene>,
+    fitness: i32,
 }
 
 impl Chromosome {
@@ -55,17 +62,18 @@ impl Chromosome {
             genes.extend(iter::repeat(Gene::from(*generator)).take(*count as usize));
         }
 
-        Self { genes }
+        Self { genes, fitness: 0 }
     }
 
-    fn mutate(&mut self) {
-        let rng = rand::RandGenerator::new();
+    fn mutate(&mut self, rng: &mut StdRng, plot: &Plot) {
+        let w = plot.width;
+        let h = plot.height;
         for genes in self.genes.iter_mut() {
-            if rng.gen_range(0.0, 1.0) < 0.5 {
+            if rng.random_bool(0.5) {
                 genes.placed = !genes.placed;
-                if genes.placed && rng.gen_range(0.0, 1.0) < 0.5 {
-                    genes.x = rng.gen_range(0, 10);
-                    genes.y = rng.gen_range(0, 10);
+                if genes.placed && rng.random_bool(0.5) {
+                    genes.x = rng.random_range(0..w);
+                    genes.y = rng.random_range(0..h);
                 }
             }
         }
@@ -76,22 +84,134 @@ impl Chromosome {
 
 struct Population {
     chromosomes: Vec<Chromosome>,
+    plot: Plot,
+    gens: Vec<(GeneratorData, u8)>,
+    rng: StdRng,
 }
 
 impl Population {
-    fn new(pop_size: usize, gens: &Vec<(GeneratorData, u8)>) -> Self {
+    fn new(pop_size: usize, gens: &Vec<(GeneratorData, u8)>, plot: &Plot, rng: StdRng) -> Self {
         let mut chromosomes = Vec::new();
 
         for i in 0..pop_size {
             chromosomes.push(Chromosome::new(gens));
         }
 
-        Self { chromosomes }
+        Self {
+            chromosomes,
+            plot: plot.clone(),
+            gens: gens.clone(),
+            rng,
+        }
     }
 
-    fn mutate(&mut self) {}
+    fn mutate(&mut self) {
+        for chromosome in self.chromosomes.iter_mut() {
+            chromosome.mutate(&mut self.rng, &self.plot);
+        }
+    }
 
-    fn crossover(&mut self) {}
+    fn crossover(&mut self) {
+        for chromosome in self.chromosomes.iter_mut() {
+            chromosome.crossover();
+        }
+    }
+
+    fn evaluate(&mut self) {
+        for chromosome in self.chromosomes.iter_mut() {
+            self.plot.evaluate_chromosome(chromosome, &self.gens);
+        }
+
+        self.chromosomes.sort_unstable_by_key(|c| -c.fitness);
+    }
+}
+
+#[derive(Clone)]
+struct Plot {
+    subplot_size: u8,
+    subplot_grid: (u8, u8),
+    multipliers: Vec<u8>,
+
+    width: u8,
+    height: u8,
+}
+
+impl Plot {
+    fn new(subplot_size: u8, subplot_grid: (u8, u8), multipliers: Vec<u8>) -> Self {
+        Self {
+            subplot_size,
+            subplot_grid,
+            multipliers,
+
+            width: subplot_size * subplot_grid.0,
+            height: subplot_size * subplot_grid.1,
+        }
+    }
+
+    fn evaluate_chromosome(
+        &self,
+        chromosome: &mut Chromosome,
+        generators: &[(GeneratorData, u8)],
+    ) -> i32 {
+        let mut fitness = 0;
+        let mut occupied = vec![false; (self.width as usize) * (self.height as usize)];
+
+        let gen_map: HashMap<u8, GeneratorData> =
+            generators.iter().map(|(g, _)| (g.id, *g)).collect();
+
+        for gene in chromosome.genes.iter() {
+            if !gene.placed {
+                continue;
+            }
+            let x = gene.x as i32;
+            let y = gene.y as i32;
+
+            let generator = gen_map.get(&gene.id).unwrap();
+
+            let w = generator.width as i32;
+            let h = generator.height as i32;
+
+            let (w, h) = if gene.rotated { (h, w) } else { (w, h) };
+
+            let value = generator.base_value as i32;
+            let area = w * h;
+
+            let w_in_plot = w.min(self.width as i32 - x);
+            let h_in_plot = h.min(self.height as i32 - y);
+
+            let area_in_plot = w_in_plot * h_in_plot;
+            let area_out_plot = area - area_in_plot;
+
+            let mut overlap_area = 0;
+            let mut base_fitness = 0;
+
+            for i in 0..w_in_plot {
+                for j in 0..h_in_plot {
+                    let cx = x + i;
+                    let cy = y + j;
+
+                    let subplot_x = cx / self.subplot_size as i32;
+                    let subplot_y = cy / self.subplot_size as i32;
+                    let subplot_idx = (subplot_y * self.subplot_grid.0 as i32 + subplot_x) as usize;
+
+                    let multiplier = self.multipliers[subplot_idx] as i32;
+                    base_fitness += value * multiplier;
+
+                    let idx = ((y + j) * self.width as i32 + (x + i)) as usize;
+                    if occupied[idx] {
+                        overlap_area += 1;
+                    }
+                    occupied[idx] = true;
+                }
+            }
+
+            let outside_penalty = area_out_plot * value;
+            let overlap_penalty = overlap_area * value;
+            fitness += (base_fitness - outside_penalty - overlap_penalty) / area;
+        }
+        chromosome.fitness = fitness;
+        fitness
+    }
 }
 
 #[macroquad::main("Population Bounds")]
@@ -103,38 +223,86 @@ async fn main() {
     gens.push((gen_1, 10));
     gens.push((gen_2, 5));
 
-    let mut population = Population::new(10, &gens);
+    let plot = Plot::new(7, (3, 4), vec![0, 0, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1]);
 
-    for i in 0..100 {
-        population.mutate();
-        population.crossover();
-    }
+    let rng = StdRng::seed_from_u64(31415926535);
 
-    let mut chromosome = population.chromosomes[0].clone();
-    chromosome.genes[0].placed = true;
-    chromosome.genes[0].rotated = true;
-    chromosome.genes[0].x = 10;
-    chromosome.genes[0].y = 10;
+    let population = Population::new(10, &gens, &plot, rng);
 
-    render_population(&chromosome, &gens).await;
+    let shared_pop = Arc::new(Mutex::new(population));
+    let thread_pop = shared_pop.clone();
+
+    thread::spawn(move || {
+        for _ in 0..100 {
+            let mut pop = thread_pop.lock().unwrap();
+            pop.mutate();
+            pop.crossover();
+            pop.evaluate();
+            drop(pop);
+        }
+    });
+
+    render_population(shared_pop, &gens, plot.width, plot.height).await;
 }
-
-async fn render_population(chromosome: &Chromosome, gens: &[(GeneratorData, u8)]) {
+async fn render_population(
+    shared_pop: Arc<Mutex<Population>>,
+    gens: &[(GeneratorData, u8)],
+    plot_width: u8,
+    plot_height: u8,
+) {
     let gen_map: HashMap<u8, GeneratorData> = gens.iter().map(|(g, _)| (g.id, *g)).collect();
+    let scale = 17.0;
 
-    let scale = 50.0;
-    let bound_width = 10.0;
-    let bound_height = 10.0;
+    let mut current_index = 0;
 
     loop {
         clear_background(GRAY);
 
-        // Draw bounding box
+        let pop = shared_pop.lock().unwrap();
+        let pop_size = pop.chromosomes.len();
+
+        let subplot_size = pop.plot.subplot_size as f32;
+        let grid_w = pop.plot.subplot_grid.0 as usize;
+        let grid_h = pop.plot.subplot_grid.1 as usize;
+
+        for grid_y in 0..grid_h {
+            for grid_x in 0..grid_w {
+                let idx = grid_y * grid_w + grid_x;
+                let multiplier = pop.plot.multipliers[idx];
+
+                let mut bg_color = match multiplier {
+                    4 => RED,
+                    3 => YELLOW,
+                    2 => LIME,
+                    1 => GREEN,
+                    0 => GRAY,
+                    _ => WHITE,
+                };
+                bg_color.a = 0.5;
+
+                let px = grid_x as f32 * subplot_size * scale;
+                let py = grid_y as f32 * subplot_size * scale;
+                let p_size = subplot_size * scale;
+
+                draw_rectangle(px, py, p_size, p_size, bg_color);
+                draw_rectangle_lines(px, py, p_size, p_size, 1.0, DARKGRAY);
+            }
+        }
+
+        if is_key_pressed(KeyCode::Right) && current_index < pop_size - 1 {
+            current_index += 1;
+        }
+        if is_key_pressed(KeyCode::Left) && current_index > 0 {
+            current_index -= 1;
+        }
+
+        let chromosome = &pop.chromosomes[current_index];
+
         draw_rectangle_lines(
             0.0,
             0.0,
-            bound_width * scale,
-            bound_height * scale,
+            plot_width as f32 * scale,
+            plot_height as f32 * scale,
             4.0,
             BLACK,
         );
@@ -158,7 +326,7 @@ async fn render_population(chromosome: &Chromosome, gens: &[(GeneratorData, u8)]
             let (box_color, text_color) = if generator.gold {
                 (GOLD, BLACK)
             } else {
-                (BLUE, WHITE)
+                (color_u8!(0, 0, 255, 50), WHITE)
             };
 
             draw_rectangle(x, y, w * scale, h * scale, box_color);
@@ -166,14 +334,29 @@ async fn render_population(chromosome: &Chromosome, gens: &[(GeneratorData, u8)]
 
             let text = chromo.id.to_string();
             let font_size = 30.0;
-            // Offsets to roughly center the text
             let text_x = x + (w * scale * 0.5) - (font_size * 0.3);
             let text_y = y + (h * scale * 0.5) + (font_size * 0.3);
 
             draw_text(&text, text_x, text_y, font_size, text_color);
         }
 
-        // Press 'S' to save the current frame as a PNG
+        draw_text(
+            &format!("Index: {}/{}", current_index, pop_size - 1),
+            10.0,
+            550.0,
+            30.0,
+            BLACK,
+        );
+        draw_text(
+            &format!("Fitness: {}", chromosome.fitness),
+            10.0,
+            580.0,
+            30.0,
+            BLACK,
+        );
+
+        drop(pop);
+
         if is_key_pressed(KeyCode::S) {
             get_screen_data().export_png("population_render.png");
             println!("Saved image to population_render.png");
